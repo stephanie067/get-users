638a8c4b5a75fa62f494030981d8d6910d4e6336
Stephanie <stephaniedeandradefreire@gmail.com>
2026-07-16 21:04:54 -0300

    feat: adicionar interface para recurso de usuários

--- server.mjs
+++ server.mjs
@@ -1,5 +1,28 @@
 import http from 'node:http';
 
+const users = [{
+    name: 'Stephanie Andrade',
+    age: 18,
+    status: true
+}, {
+    name: 'Angelica Maria',
+    age: 55,
+    status: true
+}, {
+    name: 'Douglas Andrade',
+    age: 36,
+    status: false
+}];
+
+const usersJSON = JSON.stringify(users);
+
 http.createServer((request, response ) => {
-    response.end('Hello Client');
+    if (request.url === '/users') {
+    response.writeHead(200, { 'content-type': 'application/json' });
+    response.end(JSON.stringify(users));
+    } else {
+        response.writeHead(404, { 'content-type': 'application/json'});
+        response.end(JSON.stringify({message: 'Página não encontrada.'}));
+    }
+    
 }).listen(3000);
