b9e14fa4f317815def03af42e28b1672a3bd6997
Stephanie Freire <stephaniedeandradefreire@gmail.com>
2026-07-23 21:27:52 -0300

    refractor: mover dados simulados para diretorio dedicado

---
+++ server/src/mocks/user.mock.js
@@ -0,0 +1,16 @@
+const users = [{
+    name: 'Stephanie Andrade',
+    age: 18,
+    status: true
+}, {
+    name: 'Angelica Maria',
+    age: 55,
+    status: true
+}, {
+    name: 'Douglas Andrade',
+    age: 36,
+    status: false
+}];
+
+export default users;
+
